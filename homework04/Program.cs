62aa6089b348d5b7eecbbaade8eee3ae80cc3713
Tatyana <chaser252@yandex.ru>
2022-10-08 18:13:36 +0800

    Четвертое задание

---
+++ homework04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine ("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a%2==0)
+{
+    Console.WriteLine ("Число ЧЕТНОЕ");
+}
+else
+{
+    Console.WriteLine ("Число НЕчетное");
+}
