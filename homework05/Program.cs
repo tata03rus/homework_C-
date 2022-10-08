d7c9a06eef9b2e3635bd072ec798e12b8a3d1af1
Tatyana <chaser252@yandex.ru>
2022-10-08 21:23:36 +0800

    Пятое задание

---
+++ homework05/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine ("Получается ряд:");
+int b = a*-1;
+if (a > 0)
+{
+    while (b <= a)
+    {
+        Console.WriteLine(b);
+        b++;
+    }
+}
+else
+{
+    while (a <= b)
+    {
+        Console.WriteLine(a);
+        a++;
+    }
+}
