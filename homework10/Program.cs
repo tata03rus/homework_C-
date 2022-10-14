18af13a7e900451554e1ab275e257253fcf499fe
Tatyana <chaser252@yandex.ru>
2022-10-14 20:33:33 +0800

    Десятое задание

---
+++ homework10/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите трехзначное число");
+string i;
+int a = int.Parse(Console.ReadLine());
+if (a < 1000)
+{
+    if (a > 99)
+    {
+        i = a.ToString();
+        i = i.Remove(1,1);
+        Console.WriteLine(i);
+    }
+    else
+    {
+    Console.WriteLine ("ОШИБКА");
+    }
+}
+else
+    {
+    Console.WriteLine ("ОШИБКА");
+    }
