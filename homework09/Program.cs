cea54d94ac51b40c000453790aaf34cd66dd0390
Tatyana <chaser252@yandex.ru>
2022-10-14 19:42:52 +0800

    Девятое задание

---
+++ homework09/Program.cs
@@ -0,0 +1,15 @@
+﻿int max;
+Random rnd = new Random(); //Создание объекта для генерации чисел
+int a = rnd.Next(10, 99); //Получить случайное число (в диапазоне от 0 до 10)
+Console.WriteLine(a); //Вывод числа в консоль
+int b = a/10;
+int c = a%10;
+if (b>c)
+    {
+        max = b;
+    }
+    else
+    {
+        max = c;
+    }
+Console.WriteLine(max);
