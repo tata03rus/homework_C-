5a2df94a7948321713615c1c4238a595db3151d8
Tatyana <chaser252@yandex.ru>
2022-10-15 17:06:35 +0800

    Одиннадцатое задание

---
+++ homework11/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите натуральное число");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите натуральное число");
+int b = int.Parse(Console.ReadLine());
+int c;
+if (a%b==0)
+    {
+        Console.WriteLine("Первое число кратно второму!");
+    }
+    else
+    {
+        c = a%b;
+        Console.WriteLine(c);
+    }
