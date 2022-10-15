56d453136a16f41fb18bb61973a61d5eba25e062
Tatyana <chaser252@yandex.ru>
2022-10-15 17:17:17 +0800

    Двенадцатое задание

---
+++ homework12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите число");
+string i;
+int a = int.Parse(Console.ReadLine());
+if (a > 99)
+{
+    i = a.ToString();
+    Console.WriteLine(i[2]);
+}
+    else
+    {
+    Console.WriteLine ("Третьей цифры нет");
+    }
