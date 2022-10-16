dc602616e84843220ea87ffee0d88a1e24ac0337
Tatyana <chaser252@yandex.ru>
2022-10-16 19:17:49 +0800

    Практика 2: четвертое задание

---
+++ homework2_04/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+Console.WriteLine("Введите пятизначное число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+string str = Convert.ToString(a);
+if (a > 9999 && a < 100000)
+        if (str[0] == str[4] && str[1] == str[3])
+    Console.WriteLine("Введеное число является палиндромом");
+else
+Console.WriteLine("Введеное число НЕ является палиндромом");
