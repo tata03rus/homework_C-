61dee08111401b28bb2d5ad487af9fc012645548
Tatyana <chaser252@yandex.ru>
2022-10-22 10:01:42 +0800

    Практика 3: первое задание

---
+++ homework3_01/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите любое натуральное число");
+int A = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+int result;
+
+while (i<=A)
+{
+    result = i*i*i;
+    Console.WriteLine(result);
+    i++;
+}
