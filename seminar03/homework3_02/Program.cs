b2c50fc00257f2455aca7b4ad9c87830dd108ffb
Tatyana <chaser252@yandex.ru>
2022-10-27 20:16:31 +0800

    Семинар3

---
+++ seminar03/homework3_02/Program.cs
@@ -0,0 +1,10 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите любое натуральное число");
+int a = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+while (a%10 != 0)
+{
+    sum = sum + a%10; 
+    a = a/10;
+}
+Console.WriteLine(sum);
---
+++ seminar03/homework3_04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите любое натуральное число");
+int A = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+int result;
+
+while (i<=A)
+{
+    result = i*i*i;
+    if (result % 2 == 0)
+    {
+        Console.WriteLine(result);
+    }
+    i++;
+}
