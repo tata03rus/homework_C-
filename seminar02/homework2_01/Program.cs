af934970aeb0286852824cf68be0330077f9a2e4
Tatyana <chaser252@yandex.ru>
2022-10-27 20:16:13 +0800

    Семинар2

---
+++ seminar02/homework2_01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Пусть 1 - понедельник, 2 - вторник, 3 -среда, 4 - четверг, 5 - пятница, 6 - суббота, а 7 - пятница.");
+Console.WriteLine("Введите число, обозначаюшее день недели");
+int a = int.Parse(Console.ReadLine() ?? "0");
+if (a == 6 || a == 7)
+    {
+        Console.WriteLine("Выходной день");
+    }
+else
+Console.WriteLine("Будний день");
---
+++ seminar02/homework2_02/Program.cs
@@ -0,0 +1,10 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Является ли первое число квадратом второго?");
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+if (a==b*b)
+    Console.Write("Первое число является квадратом второго");
+else
+Console.Write("Первое число НЕ является квадратом второго");
