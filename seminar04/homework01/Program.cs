3b20d9059275f742a3418148f6592b0e63bca9bc
Tatyana <chaser252@yandex.ru>
2022-10-27 20:09:03 +0800

    Папки структурированны

---
+++ seminar04/homework01/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int [] array = new int [8];
+for(int i=0; i<array.Length; i++)
+array [i] = new Random ().Next(0,2);
+Console.WriteLine(string.Join(",", array));
