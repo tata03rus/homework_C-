f793997e9ed51e73595d7c5fd5b5e569d2da809d
Tatyana <chaser252@yandex.ru>
2022-11-05 10:27:29 +0800

    задание 3

---
+++ seminar05/homework03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу копирования массива
+
+Console.WriteLine ("Введите длину массива: ");
+int n = int.Parse (Console.ReadLine() ?? "");
+
+Console.WriteLine("Задайте массив: ");
+int [] a = new int [n];
+for (int i = 0; i < n; i++)
+    a[i] = int.Parse(Console.ReadLine() ?? "");
+int[] copy = new int[a.Length];
+a.CopyTo(copy, 0);
+Console.WriteLine(String.Join(", ", copy));
