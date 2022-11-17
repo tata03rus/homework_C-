86f66f8f47a0d239da8e2c69d78932897f167094
Tatyana <chaser252@yandex.ru>
2022-11-17 20:55:32 +0800

    задание 1

---
+++ seminar09/final_work01/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Показать натуральные числа от M до N");
+Console.WriteLine();
+Console.Write("N = ");
+int N = int.Parse (Console.ReadLine() ?? "");
+Console.Write("M = ");
+int M = int.Parse (Console.ReadLine() ?? "");
+
+Console.WriteLine ("Получаем ряд: ");
+while (N < M)
+{
+    Console.Write(N + " ");
+    N++;
+}
+while (N >= M)
+{
+    Console.Write(M + " ");
+    M++;
+}
