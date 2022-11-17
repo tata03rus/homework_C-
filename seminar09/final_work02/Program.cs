c08188dff386aa346d7c7aee174620fe24c1a59e
Tatyana <chaser252@yandex.ru>
2022-11-17 21:06:22 +0800

    задание 2

---
+++ seminar09/final_work02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.WriteLine("Найти сумму элементов от M до N");
+Console.WriteLine();
+Console.WriteLine("N = 15");
+int N = 15;
+Console.WriteLine("M = 31 ");
+int M = 31;
+int sum = 0;
+
+Console.WriteLine ("Сумма равна: ");
+while (N <= M)
+{
+    sum = sum+N;
+    N++;
+}
+Console.Write(sum);
